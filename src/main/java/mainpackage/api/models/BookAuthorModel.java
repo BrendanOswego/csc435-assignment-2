73e85c164f11ee6fb3c8236925e29fdb6707218d
Brendan Goldberg <bgoldber@oswego.edu>
2018-02-17 02:22:29 -0500

    add JsonProperty annotations to BookAuthor constructor

--- src/main/java/mainpackage/api/models/BookAuthorModel.java
+++ src/main/java/mainpackage/api/models/BookAuthorModel.java
@@ -2,13 +2,15 @@ package mainpackage.api.models;
 
 import java.util.UUID;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class BookAuthorModel {
 
   private UUID ba_id;
   private UUID book_id;
   private UUID author_id;
 
-  public BookAuthorModel(UUID book_id, UUID author_id) {
+  public BookAuthorModel(@JsonProperty("book_id") UUID book_id, @JsonProperty("author_id") UUID author_id) {
     this.ba_id = UUID.randomUUID();
     this.book_id = book_id;
     this.author_id = author_id;
